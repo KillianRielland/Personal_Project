a4269c6cf51442efd4f8b037f1c21fbcea7c9926
Killian <k.rielland@yahoo.fr>
2018-04-10 13:37:49 +0200

    delete useless file

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('#burger').click(function(){
-		$(this).toggleClass('open');
-			$('#nav').toggleClass('open');
-			$('header').toggleClass('open');
-			$('aside').toggleClass('open');
-	});
-});
